9ae225ffc5a336157bfd19814c6ec42c01de4c7b
rafaelmoreira <rafaeltimoreirass@gmail.com>
2020-03-29 04:09:00 -0300

    Back-end test

---
+++ tests/integration/ong.spec.js
@@ -0,0 +1,30 @@
+const request = require ('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+  
+  it('should be able to create a new ONG', async () => {
+    const response = await request(app)
+    .post('/ongs')
+    .set('Authorization', 'ce9ee325')
+    .send({
+        name: "APAE2",
+        email: "contato@apae.com.br",
+        whatsapp: "61986348008",
+        city: "Brasília",
+        uf: "DF"
+    });
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
+  });
+});
